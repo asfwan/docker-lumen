f5b0a5db330634a0ffe3ec5212b10f8719f25a42
saf <saf@pahanggo.com>
2021-01-21 00:25:45 +0800

    default 3307 for mysql port

--- setup.sh
+++ setup.sh
@@ -2,3 +2,6 @@
 docker run --rm --interactive --tty \
   --volume $PWD/images/php:/app \
   composer create-project --prefer-dist laravel/lumen app
+
+cp .env.docker.example .env
+cp .env.app.example images/php/app/.env
